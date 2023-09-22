cc4030cd1f33e35b47fb2a0c060a3e02914e6729
Bobman76 <144392127+Bobman76@users.noreply.github.com>
2023-09-22 13:32:46 -0700

    Update t1zL-first-Javascript.js

--- t1zL-first-Javascript.js
+++ t1zL-first-Javascript.js
@@ -1,23 +1,19 @@
-  let myOutArray = new Array();
-x = 20;
+let myOutArray = [];
 
-  function myPrint(x) {
-    let myAnimal;
-    for (let i = 1; i <= x; i++) {
-      myAnimal = 'cat';
-      if (i <= 4) {
-        myAnimal = 'kitten';
-      }
-      Else {
-        myAnimal = 'cat'; 
-      } 
-      If(1 <= 12) 
-      { 
-        myAnimal = 'long cat';
-
-      }
-      myOutArray[i] = 'My' + myAnimal + ' is ' + i + ' years old <br>';
+function myPrint(x) {
+  let myAnimal;
+  for (let i = 1; i <= x; i++) {
+    myAnimal = 'cat';
+    if (i <= 4) {
+      myAnimal = 'kitten';
+    } else if (i <= 12) {
+      myAnimal = 'long cat';
     }
-    document.write(myOutArray[1] + myOutArray[2]);
-  }
 
+    myOutArray[i] = 'My ' + myAnimal + ' is ' + i + ' years old <br>';
+  }
+  document.write(myOutArray[1] + myOutArray[2]);
+}
+
+let x = 20;
+myPrint(x);
