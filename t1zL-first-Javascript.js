0d8ebe1ca6cd50378cf3fc726473349ffef60d74
Bobman76 <144392127+Bobman76@users.noreply.github.com>
2023-09-25 12:52:21 -0700

    Update t1zL-first-Javascript.js

--- t1zL-first-Javascript.js
+++ t1zL-first-Javascript.js
@@ -1,19 +1,22 @@
 let myOutArray = [];
 let x = parseInt(prompt("How old is the cat?: "));
 
-function myPrint(x) {
-  let myAnimal;
-  for (let i = 1; i <= x; i++) {
-    myAnimal = 'cat';
-    if (i <= 4) {
-      myAnimal = 'kitten';
-    } else if (i >= 12) {
-      myAnimal = 'long cat';
+if (Number.isInteger(x)) {
+  function myPrint(x) {
+    let myAnimal;
+    for (let i = 1; i <= x; i++) {
+      myAnimal = 'cat';
+      if (i <= 4) {
+        myAnimal = 'kitten';
+      } else if (i >= 12) {
+        myAnimal = 'long cat';
+      }
+
+      myOutArray[i] = 'My ' + myAnimal + ' is ' + i + ' years old <br>';
+      document.write(myOutArray[i]);
     }
-
-    myOutArray[i] = 'My ' + myAnimal + ' is ' + i + ' years old <br>';
-    document.write(myOutArray[i]);
   }
+  myPrint(x);
+} else {
+  console.log("Please input a number");
 }
-
-myPrint(x);
