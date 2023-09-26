d454017cc8708cd102de176e8080b9fc3bfe3a7c
Bobman76 <144392127+Bobman76@users.noreply.github.com>
2023-09-26 10:56:13 -0700

    Update t1zL-first-Javascript.js

--- t1zL-first-Javascript.js
+++ t1zL-first-Javascript.js
@@ -1,21 +1,43 @@
-let myOutArray = [];
-let x = parseInt(prompt("How old is the cat?: "));
-if (Number.isInteger(x)) {
-  function myPrint(x) {
-    let myAnimal;
-    for (let i = 1; i <= x; i++) {
-      myAnimal = 'cat';
-      if (i <= 4) {
-        myAnimal = 'kitten';
-      } else if (i >= 40) {
-        myAnimal = 'Immortal cat';
-      }
+<html>
+<head>
+    <title>Cat Antics</title>
+    <style>
+        body {
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+            background-image: url("");
+            background-repeat: no-repeat;
+            background-position: center top;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    <input type="button" value="click" onclick="myPrint(true)">
+    <script>
+        let myOutArray = [];
 
-      myOutArray[i] = 'My ' + myAnimal + ' is ' + i + ' years old <br>';
-      document.write(myOutArray[i]);
-    }
-  }
-  myPrint(x);
-} else {
-  alert("Input a number!");
-}
+        function myPrint(isTrue) {
+            if (isTrue) {
+                let x = parseInt(prompt("How old is the cat?: "));
+                let myAnimal;
+                for (let i = 1; i <= x; i++) {
+                    myAnimal = 'cat';
+                    if (i <= 4) {
+                        myAnimal = 'kitten';
+                    } else if (i >= 40) {
+                        myAnimal = 'Immortal cat';
+                    }
+
+                    myOutArray[i] = 'My ' + myAnimal + ' is ' + i + ' years old <br>';
+                    document.write(myOutArray[i]);
+                }
+            } else {
+                document.write("Button not clicked.");
+            }
+        }
+    </script>
+</body>
+</html>
